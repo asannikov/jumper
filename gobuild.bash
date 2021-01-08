dfcb425f0119606aadde5fd6b7d27a1189d51a48
Anton Sannikov <anton.sannikov@gmail.com>
2021-01-09 00:47:54 +0100

    Add linux/darwin/windows into platforms

---
+++ gobuild.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# https://www.digitalocean.com/community/tutorials/how-to-build-go-executables-for-multiple-platforms-on-ubuntu-16-04
+
+package=github.com/asannikov/jumper
+package_name=jumper
+
+platforms=("linux/amd64" "linux/386" "darwin/amd64" "windows/amd64" "windows/386")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name=$package_name'-'$GOOS'-'$GOARCH
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi
+
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+done
